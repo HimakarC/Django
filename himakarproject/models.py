10b3125077f4b1633fb71cf114c9c8d1748a90f7
HimakarC <116370824+HimakarC@users.noreply.github.com>
2023-09-03 15:27:47 +0530

    Delete himakarproject directory

--- himakarproject/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class destination(models.Model):
-    name = models.CharField(max_length = 100)
-    price = models.IntegerField()
-    img = models.ImageField(upload_to = 'pics')
--- himakarproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import destination
-
-# Create your views here.
-
-def himakar(request):
-    d1 = destination.objects.all()
-    return render(request,"index.html",{"d":d1})
